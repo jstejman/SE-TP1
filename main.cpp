04ea0bdec6d8b927b31c9e29c5d30c63fb164d76
Julián Stejman <jstejman@gmail.com>
2025-04-02 09:35:55 -0300

    Código terminado, falta documentar

--- main.cpp
+++ main.cpp
@@ -13,6 +13,28 @@ int main()
     
     while(true) 
     {
+        //Leer humedad
         humedadActual = humedad.read(); 
+        
+        // Condiciones para iniciar riego
+        if (!riegoManual && humedadActual < umbralHumedad) {
+            bomba = 1;
+            ledRiego = 1;
+        } else if(!riegoManual) {
+            bomba = 0;
+            ledRiego = 0;
+        }
+        
+        // Parpadeo de LED
+        ledActivo = !ledActivo;
+        // Salida UART
+        printf(
+            "Riego: %s | Botón: %s | Humedad: %.2f \n",
+            (bomba == 1) ? "Activo" : "Inactivo",
+            riegoManual = "Activado" : "Desactivado",
+            humedadActual 
+        );
+        // Esperar 1 segundo para reiniciar el ciclo
+        ThisThread::sleep_for(1000);
     }
 }
